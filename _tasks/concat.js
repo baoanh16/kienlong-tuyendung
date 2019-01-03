a1810693a923c5a29944613510b1004356ef61d7
ps06347 <vanhoangbao@gmail.com>
2019-01-03 16:13:35 +0700

    Initial commit

---
+++ _tasks/concat.js
@@ -0,0 +1,27 @@
+import fs from 'graceful-fs';
+import gulp from 'gulp'
+import concat from 'gulp-concat'
+import cssnano from 'gulp-clean-css';
+import uglifyes from 'gulp-uglify-es'
+
+
+
+gulp.task('concat_css', () => {
+	let plugins = JSON.parse(fs.readFileSync('./plugins.json'));
+	return gulp.src(plugins.styles, {
+		allowEmpty: true,
+	})
+		.pipe(concat('core.min.css'))
+		.pipe(cssnano())
+		.pipe(gulp.dest('./dist/css'))
+})
+
+gulp.task('concat_js', () => {
+	let plugins = JSON.parse(fs.readFileSync('./plugins.json'));
+	return gulp.src(plugins.scripts, {
+		allowEmpty: true,
+	})
+		.pipe(concat('core.min.js'))
+		.pipe(uglifyes())
+		.pipe(gulp.dest('./dist/js'))
+})
