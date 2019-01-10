19cc5d82db1d2be163f7ac373e510b29418ee9f3
ps06347 <vanhoangbao@gmail.com>
2019-01-10 09:09:53 +0700

    update 1 đống

--- scripts/main.js
+++ scripts/main.js
@@ -20,34 +20,70 @@ const hotline = new MappingListener({
 
 
 $(document).ready(function () {
-	
-    $('.dropdown').on('click',function(){
-        $('.dropdown-menu').toggleClass('show')
-	})
-	
-    $('.has-sub').on('click',function(){
-        $('.sub-menu').toggleClass('show')
-    })
 
-    $('.kienlong-header-1 .username').on('click',function(){
-        $('.kienlong-header-1 .user-management').slideToggle()
-        $('.kienlong-header-1 .bottom-nav').removeClass('active')
-    })
+	$('.dropdown').on('click', function () {
+		$('.dropdown-menu').toggleClass('show')
+	})
 
-    $('.kienlong-header-1 .menu-toggle').on('click',function(){
-        $('.kienlong-header-1 .bottom-nav').toggleClass('active')
+	$('.has-sub').on('click', function () {
+		$('.sub-menu').toggleClass('show')
 	})
-	
-	$('.friend-sharing').iziModal({
-		radius: 0,
-		transitionIn: "fadeInDown",
-		transitionOut: "fadeOutUp"
+
+	$('.has-sub').on('mouseover', function () {
+		$('.sub-menu').addClass('show')
 	})
+	$('.has-sub').parent().siblings().on('mouseover', function () {
+		$('.sub-menu').removeClass('show')
+	})
+	$('.sub-menu').on('mouseover', function () {
+		$('.sub-menu').addClass('show')
+	})
+	$('.sub-menu').on('mouseout', function () {
+		$('.sub-menu').removeClass('show')
+	})
+
+	$('.faq-item .answer').slideUp()
+	$('.faq-item .question').on('click', function () {
+		$(this).next().slideToggle()
+		$(this).toggleClass('active')
+		$(this).parent().siblings().children('.question').next().slideUp()
+		$(this).parent().siblings().children('.question').removeClass('active')
+	})
+	if ($(window).width() > 992) {
+		$('.kienlong-quytrinhtuyendung-1 .items-list .item .title').on('mouseover', function () {
+			$(this).next().show()
+			$(this).parent().siblings().children('.content').hide()
+		})
+	}
+
+	$('.kienlong-header-1 .username').on('click', function () {
+		$('.kienlong-header-1 .user-management').slideToggle()
+		$('.kienlong-header-1 .bottom-nav').removeClass('active')
+	})
+
+	$('.kienlong-header-1 .menu-toggle').on('click', function () {
+		$('.kienlong-header-1 .bottom-nav').toggleClass('active')
+		$('.kienlong-header-1 .user-management').slideUp()
+	})
+	if ($('.friend-sharing').length != 0) {
+		$('.friend-sharing').iziModal({
+			radius: 0,
+			transitionIn: "fadeInDown",
+			transitionOut: "fadeOutUp"
+		})
+	}
+
 	$(document).on('click', '.kienlong-tuyendung-ct .btn-share', function (event) {
 		event.preventDefault();
 		$('.friend-sharing').iziModal('open');
 	});
+
 	$(document).on('click', '.friend-sharing .modal-close', function (event) {
 		$('.friend-sharing').iziModal('close');
 	});
+
+	$(document).on('click', '.nav-qlhs-toggle', function (event) {
+		$('.nav-qlhs').slideToggle()
+	});
+
 });
